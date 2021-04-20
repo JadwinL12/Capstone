8e3f796a4b8d7502f325a474b7c06c82ec56be40
lamcal <lamcal@oregonstate.edu>
2021-04-20 01:46:03 -0500

    NoteScroll and HealthBar Implement
    Created basic Main Menu system:
     -- Functional 'Play' button
     -- Functional 'Exit' button
     -- 'Options' button not functional
    
    Created basic note objects:
     -- Basic board to hold notes
     -- Green, Red, Blue, Yellow note
     objects
     -- Functional auto scroll feature
    
    Created basic Health Bar UI:
     -- Currently health can only be loss via
     Spacebar press

---
+++ Assets/Scripts/NoteScroll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteScroll : MonoBehaviour
+{
+    public HealthBar healthBar;
+
+    public float beatTempo;
+    public bool hasStarted;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+        beatTempo = beatTempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        } else
+        {
+            transform.position -= new Vector3(0f, 0f, beatTempo * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                LoseHealth(20);
+            }
+        }
+    }
+
+    void LoseHealth(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+    }
+}
