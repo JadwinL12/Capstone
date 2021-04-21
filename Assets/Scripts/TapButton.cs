7c66e918454423e5b7eb6a1d36bd6e0e192de5e8
JadwinL12 <jadwinlin@gmail.com>
2021-04-20 19:29:33 -0700

    Created Note Buttons and Combo Bar

---
+++ Assets/Scripts/TapButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TapButton : MonoBehaviour
+{
+    public MeshRenderer mR;
+    public Material colorNotTapped;
+    public Material colorTapped;
+    public float tapped = 0.07f;
+    public float unTapped = -0.07f;
+
+    void OnMouseDown()
+    {
+        mR.material = colorTapped;
+        transform.position = new Vector3(transform.position.x, tapped, transform.position.z);
+    }
+
+    private void OnMouseUp()
+    {
+        mR.material = colorNotTapped;
+        transform.position = new Vector3(transform.position.x, unTapped, transform.position.z);
+    }
+
+}
