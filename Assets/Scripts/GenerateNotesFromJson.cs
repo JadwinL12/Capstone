5ff00fc8fab448103b0e6db7e5227b36bf3fb8ac
samxkim <36899727+samxkim@users.noreply.github.com>
2021-04-26 18:52:32 -0700

    Generated notes show but don't scroll and are not symmetrical to the default buttons

--- Assets/Scripts/GenerateNotesFromJson.cs
+++ Assets/Scripts/GenerateNotesFromJson.cs
@@ -27,22 +27,43 @@ public class Note
 public class GenerateNotesFromJson: MonoBehaviour
 {
     [FormerlySerializedAs("FilePath")] public string filePath;
+    public GameObject myPrefab;
+
+    public GameObject newobj;
+    
     // Start is called before the first frame update
-    void Start ()
+    private void Start ()
     {
-        Debug.Log("HHUH");
         filePath = Directory.GetCurrentDirectory();
 
         filePath += @"\Assets\Sounds\test.json";
         Debug.Log(filePath);
         Debug.Log("p");
-        string json_contents = File.ReadAllText(filePath);
-        MainData fumen = JsonUtility.FromJson<MainData>(json_contents);
-        Debug.Log("peee");
-        Debug.Log("name: " + fumen.name);
-        Debug.Log("maxBlock: " + fumen.maxBlock);
-        Debug.Log("BPM: " + fumen.BPM);
-        Debug.Log("offset: " + fumen.offset);
-        Debug.Log("LPB: " + fumen.notes[0].LPB);
+        var jsonContents = File.ReadAllText(filePath);
+        var mainData = JsonUtility.FromJson<MainData>(jsonContents);
+        Debug.Log("name: " + mainData.name);
+        Debug.Log("maxBlock: " + mainData.maxBlock);
+        Debug.Log("BPM: " + mainData.BPM);
+        Debug.Log("offset: " + mainData.offset);
+        Debug.Log("LPB: " + mainData.notes[0].LPB);
+        Debug.Log("Amount of Notes: " + mainData.notes.Count);
+        foreach (var note in mainData.notes)
+        {
+            double block = note.block;
+            Debug.Log("Note: " + note.num);
+            Debug.Log("Block: " + note.block);
+            block = block switch
+            {
+                1 => -3.6,
+                2 => -1.2,
+                3 => 1.2,
+                4 => 3.6,
+                _ => 1.2
+            };
+            // no way this works
+            var newobj = Instantiate (myPrefab, 
+                new Vector3 ((float) block, (float) 0.1, (float) note.num),
+                Quaternion.identity);
+        }
     }
 }
